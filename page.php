03e3bddcc827c8f2e15d6ada7d12b99610b33a17
Vio Szabo <vio@vioszabo.se>
2024-07-09 12:00:42 +0200

    update page template

--- page.php
+++ page.php
@@ -1,16 +1,27 @@
 <?php
+/*
+Template Name: Page with Sidebar
+*/
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<?php
-
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-		?>
-	</main>
+<section class="page-section">
+    <main id="primary" class="site-main">
+        <?php
+            the_post();
+            // Display the image
+            if ( has_post_thumbnail() ) {
+                the_post_thumbnail( 'full', array( 'class' => 'full-width-image' ) );
+            }
+            get_template_part( 'template-parts/content', 'page' );
+        ?>
+    </main>
+    <aside class="page-sidebar">
+        <div class="sidebar-content">
+            <?php echo get_post_meta( get_the_ID(), 'custom_sidebar_content', true ); ?>
+        </div>
+    </aside>
+</section>
 
 <?php
 get_footer();
