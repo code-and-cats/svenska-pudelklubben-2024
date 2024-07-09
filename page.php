272b3d52d62c4e9821704b152de93141213c4d67
Vio Szabo <vio@vioszabo.se>
2024-07-09 11:35:51 +0200

    remove comments

--- page.php
+++ page.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package SPK24
- */
-
 get_header();
 ?>
 
@@ -23,7 +10,7 @@ get_header();
 
 			get_template_part( 'template-parts/content', 'page' );
 		?>
-	</main><!-- #main -->
+	</main>
 
 <?php
 get_footer();
