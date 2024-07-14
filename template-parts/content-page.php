f43c4d39e53d59aea5a737bed5c8ec78dbb38cc2
Vio Szabo <vio@vioszabo.se>
2024-07-14 18:27:11 +0200

    small changes

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,7 +12,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	</header>
+	<!-- .entry-header -->
 
 	<?php the_post_thumbnail(); ?>
 
@@ -20,12 +21,7 @@
 		<?php
 		the_content();
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'spk24' ),
-				'after'  => '</div>',
-			)
-		);
+	
 		?>
 	</div><!-- .entry-content -->
 
