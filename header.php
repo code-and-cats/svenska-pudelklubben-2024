8626728feb02e3169abdb3cd93bb97cf088b2d06
Vio Szabo <vio@vioszabo.se>
2024-07-14 19:28:49 +0200

    remove previous menu

--- header.php
+++ header.php
@@ -20,8 +20,7 @@
 </div>
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'spk24' ); ?></button>
-			<?php
+						<?php
 			wp_nav_menu(
 				array(
 					'theme_location' => 'menu-1',
