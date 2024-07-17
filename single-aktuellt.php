84c5ffe25d660cdcfd2fae11fd138f68b212ee31
Vio Szabo <vio@vioszabo.se>
2024-07-17 11:33:25 +0200

    update aktuellt template

--- single-aktuellt.php
+++ single-aktuellt.php
@@ -1,15 +1,42 @@
-<?php 
+<?php
+/*
+Template Name: Page with Sidebar
+*/
 get_header();
+?>
 
-while(have_posts()) {
-  the_post(); ?>
-<div class="hero-wrapper" style="background-image: url(<?php echo get_theme_file_uri('/img/Vit-Mellanpudel-Valp-Matilda-Einebrant-8.jpg') ?>);"></div>
-<article class="article-card">
-<h1 class="page-h1"><?php the_title(); ?></h1>
-<?php the_content(); ?>
-</article>
+<section class="page-section">
+<?php
+    // Get the custom field value
+    $toppbild = get_field('toppbild');
+    
+    // Determine the top image URL
+    if ($toppbild) {
+        $top_image_url = $toppbild['url'];
+    } else {
+        // Fallback to featured image if ACF field is not set
+        $top_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+    }
+    ?>
+ <?php if ($top_image_url) : ?>
+        <img src="<?php echo esc_url($top_image_url); ?>" alt="Top Image" class="page-img" />
+    <?php endif; ?>
+    <main id="primary" class="page-main">
+        <?php
 
-<?php 
-}
+           get_template_part( 'template-parts/content', 'page' );
+            if ( has_post_thumbnail() ) {
+                the_post_thumbnail( 'full', array( 'class' => 'full-width-image' ) );
+            }
+        ?>
+    </main>
+    <aside class="page-sidebar">
+        <div class="sidebar-content">
+            <?php echo get_post_meta( get_the_ID(), 'custom_sidebar_content', true ); ?>
+        </div>
+    </aside>
+</section>
+
+<?php
 get_footer();
 ?>
