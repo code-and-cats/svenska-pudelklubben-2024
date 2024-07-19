5bead5a6af0c48c329d1b33cbad5945e4c979862
Vio Szabo <vio@vioszabo.se>
2024-07-19 09:56:37 +0200

    clean up code a lil

--- templates/single-norra.php
+++ templates/single-norra.php
@@ -18,8 +18,7 @@ get_header();
         // Fallback to featured image if ACF field is not set
         $top_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
     }
-    ?>
- <?php if ($top_image_url) : ?>
+ if ($top_image_url) : ?>
         <img src="<?php echo esc_url($top_image_url); ?>" alt="Top Image" class="page-img" />
     <?php endif; ?>
     <main id="primary" class="page-main">
