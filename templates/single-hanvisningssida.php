6faf8c63cd729a02a320c9680e64c2d84a8f7fb6
Vio Szabo <vio@vioszabo.se>
2024-08-05 13:29:29 +0200

    add more divs

--- templates/single-hanvisningssida.php
+++ templates/single-hanvisningssida.php
@@ -6,15 +6,19 @@ get_header();
 ?>
 <section>
   <div class="hanvisning-wrapper">
-<div class="hanvisning-right"><h1 class="hanvisning-h1">Valphänvisning</h1>
+<div class="hanvisning-right">
+  <div class="hanvisningscontent"><h1 class="hanvisning-h1">Valphänvisning</h1>
 <p>Det är respektive uppfödares ansvar att inskickade uppgifter är  korrekta och att kullen följer våra valphänvisningsregler. På SKKs  avelsdata och hunddata kan du kontrollera individers hälsotester och  tävlingsmeriter m.m. sök med hjälp av hundens namn eller  registreringsnummer.</p></div>
-  <div class="hanvisning-left">
+</div>  
+<div class="hanvisning-left">
+<div class="hanvisningscontent">
   <p>Det är respektive uppfödares ansvar att inskickade uppgifter är korrekta och att kullen följer våra valphänvisningsregler. På SKKs avelsdata och hunddata kan du kontrollera individers hälsotester och tävlingsmeriter med mera. Sök med hjälp av hundens namn eller registreringsnummer.</p>
   <p>Vår valphänvisare når du på: valpformedling@pudelklubben.se</p>
 <p>Svenska pudelklubbens valphänvisningsregler</p>
 <p>SKK avelsdata</p>
 <p>SKK hunddata</p>
 <p>För uppfödare - lägg in kull</p>
+</div>
 </div>
 </div>
 <div class="puppies">
