ef7b1f47f4971645802439dccf590229e24c88b3
Vio Szabo <vio@vioszabo.se>
2024-08-05 12:40:00 +0200

    style hänvisningssida

--- templates/single-hanvisningssida.php
+++ templates/single-hanvisningssida.php
@@ -6,7 +6,8 @@ get_header();
 ?>
 <section>
   <div class="hanvisning-wrapper">
-<div class="hanvisning-right"><h1 class="hanvisning-h1">Valphänvisning</h1></div>
+<div class="hanvisning-right"><h1 class="hanvisning-h1">Valphänvisning</h1>
+<p>Det är respektive uppfödares ansvar att inskickade uppgifter är  korrekta och att kullen följer våra valphänvisningsregler. På SKKs  avelsdata och hunddata kan du kontrollera individers hälsotester och  tävlingsmeriter m.m. sök med hjälp av hundens namn eller  registreringsnummer.</p></div>
   <div class="hanvisning-left">
   <p>Det är respektive uppfödares ansvar att inskickade uppgifter är korrekta och att kullen följer våra valphänvisningsregler. På SKKs avelsdata och hunddata kan du kontrollera individers hälsotester och tävlingsmeriter med mera. Sök med hjälp av hundens namn eller registreringsnummer.</p>
   <p>Vår valphänvisare når du på: valpformedling@pudelklubben.se</p>
