ef8291a6acc64498c71e5309d0bfd2d9d0f47163
Vio Szabo <vio@vioszabo.se>
2024-08-06 12:46:19 +0200

    change valphänvisning

--- templates/single-hanvisningssida.php
+++ templates/single-hanvisningssida.php
@@ -7,17 +7,17 @@ get_header();
 <section>
   <div class="hanvisning-wrapper">
 <div class="hanvisning-right">
-  <div class="hanvisningscontent"><h1 class="hanvisning-h1">Valphänvisning</h1>
-<p>Det är respektive uppfödares ansvar att inskickade uppgifter är  korrekta och att kullen följer våra valphänvisningsregler. På SKKs  avelsdata och hunddata kan du kontrollera individers hälsotester och  tävlingsmeriter m.m. sök med hjälp av hundens namn eller  registreringsnummer.</p></div>
+  <div class="hanvisningscontent"><h1 class="hanvisning-h1"><?php the_title();?></h1>
+<p>Här hittar du aktuella valpkullar inom Svenska Pudelklubben.</p></div>
 </div>  
 <div class="hanvisning-left">
 <div class="hanvisningscontent">
   <p>Det är respektive uppfödares ansvar att inskickade uppgifter är korrekta och att kullen följer våra valphänvisningsregler. På SKKs avelsdata och hunddata kan du kontrollera individers hälsotester och tävlingsmeriter med mera. Sök med hjälp av hundens namn eller registreringsnummer.</p>
   <p>Vår valphänvisare når du på: valpformedling@pudelklubben.se</p>
-<p>Svenska pudelklubbens valphänvisningsregler</p>
-<p>SKK avelsdata</p>
-<p>SKK hunddata</p>
-<p>För uppfödare - lägg in kull</p>
+<a href="#"><p>Svenska pudelklubbens valphänvisningsregler</p></a>
+<a href="#"><p>SKK avelsdata</p></a>
+<a href="#"><p>SKK hunddata</p></a>
+<a href="#"><p>För uppfödare - lägg in kull</p></a>
 </div>
 </div>
 </div>
