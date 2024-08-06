372dbfcb4c3b45dcd6241b13c16d86d7a293a013
Vio Szabo <vio@vioszabo.se>
2024-08-06 12:43:01 +0200

    add separate stylesheet for hänvisning

--- inc/scripts-styles.php
+++ inc/scripts-styles.php
@@ -8,6 +8,7 @@ function spk24_scripts() {
     wp_enqueue_style('spk_footer_styles', get_theme_file_uri('/css/footer.css'));
     wp_enqueue_style('spk_frontpage_styles', get_theme_file_uri('/css/frontpage.css'));
     wp_enqueue_style('spk_avdelningar_styles', get_theme_file_uri('/css/avdelningar.css'));
+    wp_enqueue_style('spk_hanvisning_styles', get_theme_file_uri('/css/hanvisning.css'));
     wp_enqueue_script('spk24-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 }
 add_action('wp_enqueue_scripts', 'spk24_scripts');
