433b10451afe560c19a8f4d2719ab7ee87c3bdd6
Vio Szabo <vio@vioszabo.se>
2024-08-11 21:52:20 +0200

    another change to page with sidebar

--- page.php
+++ page.php
@@ -7,37 +7,22 @@ get_header();
 
 <section class="page-section">
     <?php
-    // Get the custom field value
-    $toppbild = get_field('toppbild');
-    
-    // Determine the top image URL
-    if ($toppbild) {
-        $top_image_url = $toppbild['url'];
-    } else {
-        // Fallback to featured image if ACF field is not set
-        $top_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
-    }
-
     // Check if there is a featured image
     $has_featured_image = has_post_thumbnail();
-    ?>
-    <?php if ($top_image_url) : ?>
+    
+    // If there's a featured image, display it
+    if ($has_featured_image) : ?>
         <div class="top-image-wrapper">
-            <img src="<?php echo esc_url($top_image_url); ?>" alt="Top Image" class="top-image full-width-image" />
+            <?php the_post_thumbnail( 'full', array( 'class' => 'top-image full-width-image' ) ); ?>
         </div>
     <?php endif; ?>
     
-    <main id="primary" class="page-main">
+    <main id="primary" class="site-main">
         <?php
         // Start the loop
         if ( have_posts() ) :
             while ( have_posts() ) : the_post();
                 get_template_part( 'template-parts/content', 'page' );
-
-                // Display the featured image
-                if ( $has_featured_image ) {
-                    the_post_thumbnail( 'full', array( 'class' => 'full-width-image' ) );
-                }
             endwhile;
         endif;
         ?>
