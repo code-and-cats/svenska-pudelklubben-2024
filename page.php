3a619d510bb9055ecaa697a340f3c30f04b65476
Vio Szabo <vio@vioszabo.se>
2024-08-11 21:45:16 +0200

    add conditional check to add margin

--- page.php
+++ page.php
@@ -6,7 +6,7 @@ get_header();
 ?>
 
 <section class="page-section">
-<?php
+    <?php
     // Get the custom field value
     $toppbild = get_field('toppbild');
     
@@ -17,20 +17,33 @@ get_header();
         // Fallback to featured image if ACF field is not set
         $top_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
     }
+
+    // Check if there is a featured image
+    $has_featured_image = has_post_thumbnail();
     ?>
- <?php if ($top_image_url) : ?>
-        <img src="<?php echo esc_url($top_image_url); ?>" alt="Top Image" class="page-img" />
+    <?php if ($top_image_url) : ?>
+        <div class="top-image-wrapper">
+            <img src="<?php echo esc_url($top_image_url); ?>" alt="Top Image" class="top-image full-width-image" />
+        </div>
     <?php endif; ?>
+    
     <main id="primary" class="page-main">
         <?php
-            the_post();
-            get_template_part( 'template-parts/content', 'page' );
-            if ( has_post_thumbnail() ) {
-                the_post_thumbnail( 'full', array( 'class' => 'full-width-image' ) );
-            }
+        // Start the loop
+        if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
+                get_template_part( 'template-parts/content', 'page' );
+
+                // Display the featured image
+                if ( $has_featured_image ) {
+                    the_post_thumbnail( 'full', array( 'class' => 'full-width-image' ) );
+                }
+            endwhile;
+        endif;
         ?>
     </main>
-    <aside class="page-sidebar">
+    
+    <aside class="page-sidebar <?php echo $has_featured_image ? 'has-featured-image' : ''; ?>">
         <div class="sidebar-content">
             <?php echo get_post_meta( get_the_ID(), 'custom_sidebar_content', true ); ?>
         </div>
