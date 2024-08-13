61df1cdd142ec92bade74363cbbca8dcae7c5abe
Vio Szabo <vio@vioszabo.se>
2024-08-13 16:09:57 +0200

    add wrapper to footer

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <footer class="site-footer">
+  <div class="footer-wrapper">
 <img
   class="logo-footer"
   src="<?php echo get_theme_file_uri('/img/Svenska Pudelklubben_Logo_RGB_Red_Horisontal.png') ?>"
@@ -21,6 +22,7 @@
   <a href="https://www.facebook.com/spksvenskapudelklubben"><img class="footer-img" src="<?php echo get_theme_file_uri('/img/fa-logo.png') ?>" /></a>
   <a href="https://www.youtube.com/c/SvenskaPudelklubben"><img class="footer-img"  src="<?php echo get_theme_file_uri('/img/yt-logo.png') ?>" /></a>
   <a href="https://www.instagram.com/svenskapudelklubben/"><img class="footer-img"  src="<?php echo get_theme_file_uri('/img/instagram.png') ?>" /></a>
+</div>
 </div>
 </footer>
 
