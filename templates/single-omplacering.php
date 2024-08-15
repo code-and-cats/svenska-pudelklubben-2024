b5faa073c291b49ac34141862db07dec45765394
Vio Szabo <vio@vioszabo.se>
2024-08-15 11:30:23 +0200

    rephrase omplacering

--- templates/single-omplacering.php
+++ templates/single-omplacering.php
@@ -9,11 +9,11 @@ get_header();
 <div class="hanvisning-right">
   <div class="hanvisningscontent"><h1 class="hanvisning-h1"><?php the_title();?></h1>
 <p>Här hittar du aktuella omplaceringar inom Svenska Pudelklubben.</p>
-<p>De hundar som finns noterade på denna sida är över sex månader och är i alla storlekar. De har anmälts för hänvisning till klubbens vuxenhänvisare. Varje notis ligger i datumordning utan annan sortering. Annonsen finns på sidan i en månad, förlängning med 14 dagar i taget.</p></div>
+<p>De hundar som finns noterade på denna sida är över sex månader och är i alla storlekar. De har anmälts för hänvisning till klubbens vuxenhänvisare. Annonsen finns på sidan i en månad, förlängning med 14 dagar i taget.</p></div>
 </div>  
 <div class="hanvisning-left">
 <div class="hanvisningscontent">
-  <p>Till dig som anmäler hund för omplacering</p>
+  <h2>Till dig som anmäler hund för omplacering</h2>
     <p>Hunden ska vara registrerad i SKK.</p>
 <p>Fyll i formuläret för omplacering.</p>
   <p>För ägaren: namn, e-post, telefonnummer och medlemsnummer.
