4a692230495932b841e71a2ba9d914c51cd6bfba
Vio Szabo <vio@vioszabo.se>
2024-08-15 11:34:05 +0200

    add link omplacering

--- templates/single-omplacering.php
+++ templates/single-omplacering.php
@@ -13,11 +13,8 @@ get_header();
 </div>  
 <div class="hanvisning-left">
 <div class="hanvisningscontent">
-  <h2>Till dig som anmäler hund för omplacering</h2>
-    <p>Hunden ska vara registrerad i SKK. Fyll i formuläret för omplacering.</p>
-  <p>För ägaren: namn, e-post, telefonnummer och medlemsnummer.
-
-För hunden: ålder, storlek, kön, färg och registreringsnummer är obligatoriska uppgifter och en beskrivning av hunden är önskvärt.
+  <h3>Anmäl hund för omplacering</h3>
+    <p>Fyll i <a href="#">formuläret för omplacering</a>. Hunden ska vara registrerad i SKK. </p>
 
 <p>För annonsering av omplacering kontakta: <a href="mailto:valpformedling@pudelklubben.se">valpformedling@pudelklubben.se</a></p>
 <p>Allt arbete inom Svenska Pudelklubben sker ideellt på fritiden vilket innebär att det kan ta någon vecka innan hänvisningen publiceras. Vår målsättning är att det ska uppdateras minst 2 gånger i veckan.</a></p>
