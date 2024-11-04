297fd0fe23797dd5038cf1396e8699388d92d5e0
Vio Szabo <vio@vioszabo.se>
2024-11-04 18:55:03 +0100

    change text hänvisning

--- templates/single-hanvisningssida.php
+++ templates/single-hanvisningssida.php
@@ -14,7 +14,13 @@ get_header();
 <div class="hanvisning-left">
 <div class="hanvisningscontent">
 <h3>Regler för valphänvisning</h3>
-  <p>Det är respektive uppfödares ansvar att inskickade uppgifter är korrekta och att kullen följer våra valphänvisningsregler. På SKKs avelsdata och hunddata kan du kontrollera individers hälsotester och tävlingsmeriter med mera. Sök med hjälp av hundens namn eller registreringsnummer.</p>
+<p>Dessa annonser är publicerade och uppdaterade av uppfödarna själva. Klubben tar ner annonser när valpkullen blivit 12 veckor, eller om uppfödaren inte uppdaterat på 3 veckor.</p>
+<p>Valphänvisningsannonser får tidigast publiceras 9 veckor före beräknad födsel. Valpar får hänvisas till och med 6 månaders ålder.</p>
+
+
+  <p>Det är respektive uppfödares ansvar att inskickade uppgifter är korrekta och att kullen följer våra valphänvisningsregler. Läs igenom dessa noga! Dubbelkolla att båda föräldradjuren har de hälsotester som krävs för valphänvisning. 
+Tänk på att det för ögonlysning är en viss ålder (efter 18 mån och 5 år). Blir din annons bortplockad kan det bero på att tiden för ögonlysningen inte är korrekt.</p>
+    <p>På SKKs avelsdata och hunddata kan du kontrollera individers hälsotester och tävlingsmeriter med mera. Sök med hjälp av hundens namn eller registreringsnummer.</p>
   <p>Vår valphänvisare når du på: valpformedling@pudelklubben.se</p>
 <a href="https://pudelklubben.se/wp-content/uploads/2024/01/SPK-Valphanvisningsregler-2024.pdf"><p>Svenska pudelklubbens valphänvisningsregler</p></a>
 <a href="https://hundar.skk.se/avelsdata/Initial.aspx"><p>SKK avelsdata</p></a>
