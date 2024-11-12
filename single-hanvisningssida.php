d8808cd5ab3073fca3165718e0207971dcc7eb1e
Vio Szabo <vio@vioszabo.se>
2024-11-12 11:57:43 +0100

    temporary move of hänvisning

---
+++ single-hanvisningssida.php
@@ -0,0 +1,42 @@
+<?php
+/*
+* Template Name: Hänvisningssida
+*/
+get_header();
+?>
+<div class="main-content">
+<section>
+  <div class="hanvisning-wrapper">
+<div class="hanvisning-right">
+  <div class="hanvisningscontent"><h1 class="hanvisning-h1"><?php the_title();?></h1>
+<p>Här hittar du aktuella valpkullar inom Svenska Pudelklubben.</p>
+<p>Är du särskilt intresserad av mentalitet? Vi har samlat alla <a href="https://www.pudeltest.online/uppfodare-med-bph-diplom/">uppfödare med BPH-diplom</a> på denna sida.</p>
+<p>Läs mer om att <a href="https://www.pudeltest.online/kopapudel/">skaffa pudel på våra sidor!</a></p></div>
+</div>  
+<div class="hanvisning-left">
+<div class="hanvisningscontent">
+<h3>Regler för valphänvisning</h3>
+<p>Dessa annonser är publicerade och uppdaterade av uppfödarna själva. Klubben tar ner annonser när valpkullen blivit 12 veckor, eller om uppfödaren inte uppdaterat på 3 veckor.</p>
+<p>Valphänvisningsannonser får tidigast publiceras 9 veckor före beräknad födsel. Valpar får hänvisas till och med 6 månaders ålder.</p>
+
+
+  <p>Det är respektive uppfödares ansvar att inskickade uppgifter är korrekta och att kullen följer våra valphänvisningsregler. Läs igenom dessa noga! Dubbelkolla att båda föräldradjuren har de hälsotester som krävs för valphänvisning. 
+Tänk på att det för ögonlysning är en viss ålder (efter 18 mån och 5 år). Blir din annons bortplockad kan det bero på att tiden för ögonlysningen inte är korrekt.</p>
+    <p>På SKKs avelsdata och hunddata kan du kontrollera individers hälsotester och tävlingsmeriter med mera. Sök med hjälp av hundens namn eller registreringsnummer.</p>
+  <p>Vår valphänvisare når du på: valpformedling@pudelklubben.se</p>
+<a href="https://pudelklubben.se/wp-content/uploads/2024/01/SPK-Valphanvisningsregler-2024.pdf"><p>Svenska pudelklubbens valphänvisningsregler</p></a>
+<a href="https://hundar.skk.se/avelsdata/Initial.aspx"><p>SKK avelsdata</p></a>
+<a href="https://hundar.skk.se/hunddata/"><p>SKK hunddata</p></a>
+<a href="https://pudelklubben.se/kopa-pudel/uppfodarlogin/"><p>För uppfödare - lägg in kull</p></a>
+</div>
+</div>
+</div>
+<div class="puppies">
+<?php
+            the_content();
+?>
+</div>
+<?php
+get_footer();
+?>
+</div>
