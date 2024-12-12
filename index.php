d6e77beb674a76099b91d28d9de857e766c9ed67
Vio Szabo <vio@vioszabo.se>
2024-12-12 16:57:11 +0100

    Remove loop from index.php

--- index.php
+++ index.php
@@ -28,28 +28,6 @@ get_header();
 				<?php
 			endif;
 
-			/* Start the Loop */
-			$homepageAktuellt = new WP_Query(array(
-				'posts_per_page' => 6,
-				'post_type' => 'aktuellt'
-			));
-			while ($homepageAktuellt have_posts() ) :
-				$homepageAktuellt->the_post();
-	
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type('aktuellt') );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
 			get_template_part( 'template-parts/content', 'none' );
 
 		endif;
@@ -58,5 +36,4 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
