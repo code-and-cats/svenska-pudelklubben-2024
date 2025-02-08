a96a23a1f50713deb0937f84d1f7b85e78bc5368
Vio Szabo <vio@vioszabo.se>
2025-02-08 11:33:12 +0100

    frontpage changes

--- front-page.php
+++ front-page.php
@@ -2,12 +2,12 @@
 get_header();
 ?>
 <div class="main-content">
-<div class="hero-wrapper" style="background-image: url(<?php echo get_theme_file_uri('/img/20230222-Erika-Jansson-scaled.jpg') ?>);">
+<div class="hero-wrapper" style="background-image: url(<?php echo get_theme_file_uri('/img/dir-6.jpg') ?>);">
       <div class="hero-container">
         <h1 class="main-heading">Pudeln</h1>
           <h1 class="secondary-heading">variationernas ras</h1>
           <div class="hero-text-wrapper">
-        <p class="hero-text"><a href="https://www.pudelklubben.se/om-klubben/bli-medlem/">Bli medlem</a></p>
+        <p><a href="https://www.pudelklubben.se/om-klubben/bli-medlem/" class="hero-text">Bli medlem</a></p>
 </div>
       </div>
     </div>
@@ -20,8 +20,13 @@ get_header();
        <p class="article-text">
         Utställning, kurs eller pudelpromenad? Alla våra aktiviteter hittar du i vår kalender.
        </p>
+       <p>
        <a href="https://www.pudelklubben.se/aktiviteter/">
-       <button>Aktiviteter</button></a>
+       <button>Aktiviteter</button></a></p>
+       <p>
+       <a href="https://www.facebook.com/spksvenskapudelklubben/events">
+       <button class="aktiviteter-fb">Aktiviteter på Facebook</button></a>
+</p>
        </div>
      </article>
      <article class="article-card">
@@ -36,12 +41,12 @@ get_header();
      </article>
      <article class="article-card">
      <div class="article-content">
-       <h2 class="article-heading">Bli medlem</h2>
+       <h2 class="article-heading">Valberedningen söker dig!</h2>
        <p class="article-text">
-        Som medlem får du tillgång till alla våra aktiviteter och många bra medlemsförmåner hos våra sponsorer.
+        Just nu söker vi flera intresserade som vill bidra i klubbens centralstyrelse!
        </p>
-       <a href="https://www.pudelklubben.se/om-klubben/bli-medlem/">
-       <button>Bli medlem</button></a>
+       <a href="https://www.pudelklubben.se/valberedningen-soker-dig/">
+       <button>Läs mer</button></a>
        </div>
      </article>
      <?php
@@ -72,7 +77,6 @@ while($homepageAktuellt->have_posts()) {
    <section class="sponsor-wrapper">
     <h3 class="sponsor-heading">Våra sponsorer</h3>
     <div><a href="https://www.svedea.se/hundforsakring/hundagare/hundklubbar/svenska-pudelklubben?fbclid=IwAR33GfXUp-3UuKC1_uRRGaUbv5tCE9VlYVxVPxMpkTQP1xw7aWwRZLNI4Ug"><img class="sponsor-logo" src="<?php echo get_theme_file_uri('/img/svedea-forsakring-logotyp.png') ?>;" alt="Svedea logo"></a></div>
-    <div><a href="https://dr-claudersverige.se/"><img class="sponsor-logo" src="<?php echo get_theme_file_uri('/img/dr-clauders-sverige-960x480-560x280.jpg') ?>;" alt="Dr Clauders logo"></a></div>
     <div><a href="https://www.4dogs.se/"><img class="sponsor-logo" src="<?php echo get_theme_file_uri('/img/4Dogs-logga-genomskinlig-med-svart-text-2048x485.jpg') ?>;" alt="4dogs logo"></a></div>
     <div><a href="https://www.alwaysyourfriends.com/"><img class="sponsor-logo" src="<?php echo get_theme_file_uri('/img/alwaysyoufriend-2048x2048.png') ?>;" alt="Always your friend logo"></a></div>
     <div><a href="https://www.lyckliga.nu/"><img class="sponsor-logo" src="<?php echo get_theme_file_uri('/img/Lyckliga-Faret-Logga-2048x1034.jpg') ?>;" alt="Lyckliga fåret logo"></a></div>
